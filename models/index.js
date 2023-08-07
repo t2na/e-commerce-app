3ab8f177d0096fa8909a3996a09f4e050cbe2534
Zach Antunes <ztunes4@gmail.com>
2023-08-07 14:16:00 -0700

    can't get tags and products to show up in respective response objects

--- models/index.js
+++ models/index.js
@@ -17,11 +17,13 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
   through: 'ProductTag',
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
   through: 'ProductTag',
 })
 
