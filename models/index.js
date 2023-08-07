a5b6e40b06dc2501288779fcfb8dcb1984b08e58
Zach Antunes <ztunes4@gmail.com>
2023-08-07 14:55:49 -0700

    trying to fix models index.js syntax to get GET api/tags to work

--- models/index.js
+++ models/index.js
@@ -17,14 +17,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_id',
   through: 'ProductTag',
+  foreignKey: 'product_id',
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'tag_id',
   through: 'ProductTag',
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
